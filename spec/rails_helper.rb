d07d0b4033396fc4b186e4ef7866640ba7c06f2d
anakettl <anakettl@gmail.com>
2020-06-25 23:58:17 -0300

    Change config to rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,13 +8,6 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-   with.test_framework :rspec
-   with.library :rails
- end
-end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -40,24 +33,57 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
-RSpec.configuration do |config|
-  # [...]
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+   with.test_framework :rspec
+   with.library :rails
+ end
+end
+
+RSpec.configure do |config|
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
   # set up factory bot
   config.include FactoryBot::Syntax::Methods
 
   # set up database cleaner
   #start by truncating all the tables but then use the faster transaction strategy the rest of the time.
- config.before(:suite) do
-   DatabaseCleaner.clean_with(:truncation)
-   DatabaseCleaner.strategy = :transaction
- end
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
 
- # start the transaction strategy as examples are run
+  # start the transaction strategy as examples are run
  config.around(:each) do |example|
-   DatabaseCleaner.cleaning do
-     example.run
-   end
- end
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 
- # [...]
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = true
+
+  # RSpec Rails can automatically mix in different behaviours to your tests
+  # based on their file location, for example enabling you to call `get` and
+  # `post` in specs under `spec/controllers`.
+  #
+  # You can disable this behaviour by removing the line below, and instead
+  # explicitly tag your specs with their type, e.g.:
+  #
+  #     RSpec.describe UsersController, :type => :controller do
+  #       # ...
+  #     end
+  #
+  # The different available types are documented in the features, such as in
+  # https://relishapp.com/rspec/rspec-rails/docs
+  config.infer_spec_type_from_file_location!
+
+  # Filter lines from Rails gems in backtraces.
+  config.filter_rails_from_backtrace!
+  # arbitrary gems may also be filtered via:
+  # config.filter_gems_from_backtrace("gem name")
 end
